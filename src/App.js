15f023f4a9e59d791c9f0be7b3a3e1ca4492762f
vatsalshah041 <shahvatsal2003@gmail.com>
2023-05-28 01:14:19 +0530

    thodasa done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { DndProvider } from 'react-dnd';
+import Homepage from './pages/Homepage';
+import {HTML5Backend as Backend} from 'react-dnd-html5-backend';
+import Header from './component/Header';
 function App() {
   return (
     <>
-    
+    <DndProvider backend={Backend}>
+      <Header/>
+      <Homepage/>
+    </DndProvider>
     
     </>
   );
---
+++ src/component/Col.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Col=({isOver,children})=> {
+  const className=isOver?"highlight-region":"";
+  return (
+    <>
+    <div className={`col${className}`}>
+      {children}
+    </div>
+    </>
+  );
+};
+
+export default Col;
